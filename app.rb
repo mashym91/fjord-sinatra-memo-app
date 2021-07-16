d6f5c4a4d32590b0635477f872537f5e3708baad
mashym91 <mashym91@gmail.com>
2021-07-16 16:24:40 +0900

    一覧,登録を仮実装

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
+require 'byebug'
+require 'active_support/all'
+
+JSON_FILE = 'db/memos.json'
 
 get '/memos' do
+  unless File.exist?(JSON_FILE)
+    File.open(JSON_FILE, 'w') do |file|
+      hash = { 'memos' => [] }
+      JSON.dump(hash, file)
+    end
+  end
+
+  @memos = []
+  File.open(JSON_FILE, 'r') do |file|
+    hash = JSON.parse(file.read)
+    @memos = hash['memos'] if hash.present?
+  end
   erb :index
 end
 
@@ -14,6 +33,27 @@ get '/memos/:id' do
 end
 
 post '/memos' do
+  current_memos = []
+  File.open(JSON_FILE) do |file|
+    hash = JSON.parse(file.read)
+    current_memos = hash['memos']
+  end
+
+  memo = {}
+  memo['id'] = current_memos.present? ? current_memos.last['id'] + 1 : 1
+  memo['title'] = params[:title]
+  memo['memo'] = params[:memo]
+  memo['created_at'] = Time.now.to_s
+  memo['updated_at'] = Time.now.to_s
+  memo['deleted_at'] = nil
+
+  dump_memos = {}
+  dump_memos['memos'] = current_memos.push(memo)
+  File.open(JSON_FILE, 'w') do |file|
+    JSON.dump(dump_memos, file)
+  end
+
+  @memos = dump_memos['memos']
   erb :index
 end
 
