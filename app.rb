1b0c3770b91862d1f04e84df4e1d8acc7d8ec8e6
mashym91 <mashym91@gmail.com>
2021-07-20 15:49:25 +0900

    xss対策

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ JSON_FILE = settings.test? ? 'db/test_memos.json' : 'db/memos.json'
 
 enable :method_override
 
+helpers do
+  alias_method :h, :escape_html
+end
+
 before do
   unless File.exist?(JSON_FILE)
     File.open(JSON_FILE, 'w') do |file|
@@ -48,8 +52,8 @@ end
 post '/memos' do
   memo = {}
   memo['id'] = @memos.present? ? @memos.last['id'] + 1 : 1
-  memo['title'] = params[:title]
-  memo['memo'] = params[:memo]
+  memo['title'] = h params[:title]
+  memo['memo'] = h params[:memo]
   memo['created_at'] = Time.now.to_s
   memo['updated_at'] = Time.now.to_s
   memo['deleted_at'] = nil
@@ -68,8 +72,8 @@ end
 
 patch '/memos/:id' do
   memo = @memos.find { |m| m['id'] == params[:id].to_i }
-  memo['title'] = params[:title]
-  memo['memo'] = params[:memo]
+  memo['title'] = h params[:title]
+  memo['memo'] = h params[:memo]
   memo['updated_at'] = Time.now.to_s
 
   update_memos = {}
